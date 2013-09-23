19a4cd3fcfe24eb5f63f24c4efafe6527dc3b1c4
labarq <labarq@labarq-OptiPlex-990.(none)>
2013-09-23 19:20:48 -0300

    Adicionando Rails ao projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecomendadorEventos::Application.config.secret_key_base = 'f101111484df374e6c6c5a46059b40931b740baa84e5679c7e269eaa46b74fd1748272c2802104e4c6b7018067d24c9788a32429cc0b0a083a0dbc1e1316f816'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RecomendadorEventos::Application.config.session_store :cookie_store, key: '_recomendador-eventos_session'
