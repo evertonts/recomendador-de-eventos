64684f91430d754a522421afc4b11cceb50dcf9e
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2013-09-30 17:35:06 -0300

    Adicionado o modelo Event

---
+++ db/migrate/20130930203249_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :location
+      t.string :date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
