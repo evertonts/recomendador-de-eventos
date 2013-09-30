a553eba7bacce5c5806014023e2ac442adc59d68
Luiz Augusto Gama de Moraes <augustomoraes90@gmail.com>
2013-09-30 19:19:43 -0300

    index de eventos

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ActiveRecord::Base
-	attr_accessor :title, :location, :date, :description, :url
 	
 	validates_uniqueness_of :url 
 end
--- lib/parser_catraca_livre.rb
+++ lib/parser_catraca_livre.rb
@@ -30,4 +30,3 @@ class ParserCatracaLivre
 	end
 end
 
-
