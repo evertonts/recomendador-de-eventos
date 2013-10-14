134cb5ce8264b68d2986e31f394338ba101ad855
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2013-10-14 09:52:28 -0300

    Criado o parser para a folha

---
+++ lib/parser_folha.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'nokogiri'
+
+class ParserFolha
+	
+	def initialize()
+		@url = 'http://tools.folha.com.br/print?site=guia&url=http%3A%2F%2Fguia1.folha.com.br%2Fbusca%2Fshows%2F%3Fq%3D'
+	end
+	
+	def parse()
+	  html = Nokogiri::HTML.parse(open(@url), nil, 'iso-8859-1')
+	  html.css('#roteiro > .element').each do |article|
+			event = Event.new
+      event.title = article.css('h1 a').first.content.strip
+      event.location = article.css('.servico').first.content.strip
+      event.url = article.css('h1 a').first.attributes['href'].value
+      event.description = article.css('.editorial').first.content.strip
+      #tags = article.css('h1 span b').first.content.strip
+      event.save!
+    end
+	end
+end
+
